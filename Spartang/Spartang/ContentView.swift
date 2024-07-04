c6c51a1d78abb0090f314da87b18f96b3713c99e
Haera <youreverse@naver.com>
2024-07-04 18:34:03 +0900

    업데이트

--- Spartang/Spartang/ContentView.swift
+++ Spartang/Spartang/ContentView.swift
@@ -1,133 +1,133 @@
+////
+////  ContentView.swift
+////  Spartang
+////
+////  Created by t2023-m0019 on 7/4/24.
+////
+//import SwiftUI
 //
-//  ContentView.swift
-//  Spartang
+//struct ContentView: View {
+//    @State var currentTab: Int = 0
+//    var body: some View {
+//        ZStack(alignment: .top) {
+//            TabView(selection: self.$currentTab) {
+//                View1().tag(0)
+//                View2().tag(1)
+//                View3().tag(2)
+//                View4().tag(3)
+//                View5().tag(4)
+//            }
+//            .tabViewStyle(.page(indexDisplayMode: .always))
+//            .edgesIgnoringSafeArea(.all)
+//            
+//            TabBarView(currentTab: self.$currentTab)
+//                .padding(.top, 45)
+//        }
+//    }
+//}
 //
-//  Created by t2023-m0019 on 7/4/24.
+//struct TabBarView: View {
+//    @Binding var currentTab: Int
+//    @Namespace var namespace
+//    var tabBarOptions: [String] = ["Cart", "안주", "탕", "술", "음료"]
+//    var body: some View {
+//        //        ScrollView(.horizontal, showsIndicators: false) {
+//        HStack(spacing: 0) {
+//            ForEach(Array(zip(self.tabBarOptions.indices,
+//                              self.tabBarOptions)),
+//                    id: \.0,
+//                    content: {
+//                index, name in
+//                TabBarItem(currentTab: self.$currentTab,
+//                           namespace: namespace.self,
+//                           tabBarItemName: name,
+//                           tab: index)
+//                
+//            }
+//            )
+//            .frame(maxWidth: .infinity)
+//            .background(Color.white)
+//            .frame(width: 80)
+//            .frame(height: 80)
+//            
+//        }
+//    }
+//}
 //
-import SwiftUI
-
-struct ContentView: View {
-    @State var currentTab: Int = 0
-    var body: some View {
-        ZStack(alignment: .top) {
-            TabView(selection: self.$currentTab) {
-                View1().tag(0)
-                View2().tag(1)
-                View3().tag(2)
-                View4().tag(3)
-                View5().tag(4)
-            }
-            .tabViewStyle(.page(indexDisplayMode: .always))
-            .edgesIgnoringSafeArea(.all)
-            
-            TabBarView(currentTab: self.$currentTab)
-                .padding(.top, 45)
-        }
-    }
-}
-
-struct TabBarView: View {
-    @Binding var currentTab: Int
-    @Namespace var namespace
-    var tabBarOptions: [String] = ["Cart", "안주", "탕", "술", "음료"]
-    var body: some View {
-        //        ScrollView(.horizontal, showsIndicators: false) {
-        HStack(spacing: 0) {
-            ForEach(Array(zip(self.tabBarOptions.indices,
-                              self.tabBarOptions)),
-                    id: \.0,
-                    content: {
-                index, name in
-                TabBarItem(currentTab: self.$currentTab,
-                           namespace: namespace.self,
-                           tabBarItemName: name,
-                           tab: index)
-                
-            }
-            )
-            .frame(maxWidth: .infinity)
-            .background(Color.white)
-            .frame(width: 80)
-            .frame(height: 80)
-            
-        }
-    }
-}
-
-struct TabBarItem: View {
-    @Binding var currentTab: Int
-    let namespace: Namespace.ID
-    
-    
-    var tabBarItemName: String
-    var tab: Int
-    
-    var body: some View {
-        Button {
-            self.currentTab = tab
-        } label: {
-            VStack {
-                Spacer()
-                Text(tabBarItemName)
-                    .font(.system(size: 20))
-                if currentTab == tab {
-                    Color.black
-                        .frame(height: 2)
-                        .matchedGeometryEffect(id: "underline",
-                                               in: namespace,
-                                               properties: .frame)
-                } else {
-                    Color.clear.frame(height: 2)
-                }
-            }
-            .animation(.spring(), value: self.currentTab)
-        }
-        .buttonStyle(.plain)
-    }
-}
-
-struct View1: View {
-    var body: some View {
-        Color.red
-            .opacity(0.2)
-            .edgesIgnoringSafeArea(.all)
-    }
-}
-
-struct View2: View {
-    var body: some View {
-        Color.yellow
-            .opacity(0.2)
-            .edgesIgnoringSafeArea(.all)
-    }
-}
-
-struct View3: View {
-    var body: some View {
-        Color.green
-            .opacity(0.2)
-            .edgesIgnoringSafeArea(.all)
-    }
-}
-
-struct View4: View {
-    var body: some View {
-        Color.blue
-            .opacity(0.2)
-            .edgesIgnoringSafeArea(.all)
-    }
-}
-
-struct View5: View {
-    var body: some View {
-        Color.cyan
-            .opacity(0.2)
-            .edgesIgnoringSafeArea(.all)
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct TabBarItem: View {
+//    @Binding var currentTab: Int
+//    let namespace: Namespace.ID
+//    
+//    
+//    var tabBarItemName: String
+//    var tab: Int
+//    
+//    var body: some View {
+//        Button {
+//            self.currentTab = tab
+//        } label: {
+//            VStack {
+//                Spacer()
+//                Text(tabBarItemName)
+//                    .font(.system(size: 20))
+//                if currentTab == tab {
+//                    Color.black
+//                        .frame(height: 2)
+//                        .matchedGeometryEffect(id: "underline",
+//                                               in: namespace,
+//                                               properties: .frame)
+//                } else {
+//                    Color.clear.frame(height: 2)
+//                }
+//            }
+//            .animation(.spring(), value: self.currentTab)
+//        }
+//        .buttonStyle(.plain)
+//    }
+//}
+//
+//struct View1: View {
+//    var body: some View {
+//        Color.red
+//            .opacity(0.2)
+//            .edgesIgnoringSafeArea(.all)
+//    }
+//}
+//
+//struct View2: View {
+//    var body: some View {
+//        Color.yellow
+//            .opacity(0.2)
+//            .edgesIgnoringSafeArea(.all)
+//    }
+//}
+//
+//struct View3: View {
+//    var body: some View {
+//        Color.green
+//            .opacity(0.2)
+//            .edgesIgnoringSafeArea(.all)
+//    }
+//}
+//
+//struct View4: View {
+//    var body: some View {
+//        Color.blue
+//            .opacity(0.2)
+//            .edgesIgnoringSafeArea(.all)
+//    }
+//}
+//
+//struct View5: View {
+//    var body: some View {
+//        Color.cyan
+//            .opacity(0.2)
+//            .edgesIgnoringSafeArea(.all)
+//    }
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
--- Spartang/Spartang/ViewController.swift
+++ Spartang/Spartang/ViewController.swift
@@ -4,33 +4,33 @@
 //
 //  Created by 차해라 on 7/2/24.
 //
+
 import UIKit
 import SnapKit
 import SwiftUI
 
-class MyViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let swiftUIView = ContentView()
-        let hostingController = UIHostingController(rootView: swiftUIView)
-        
-        addChild(hostingController)
-        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(hostingController.view)
-        hostingController.didMove(toParent: self)
-        
-        hostingController.view.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-    }
-}
-
-
+//class MyViewController: UIViewController {
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        let swiftUIView = ContentView()
+//        let hostingController = UIHostingController(rootView: swiftUIView)
+//
+//        addChild(hostingController)
+//        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(hostingController.view)
+//        hostingController.didMove(toParent: self)
+//
+//        hostingController.view.snp.makeConstraints { make in
+//            make.edges.equalToSuperview()
+//        }
+//    }
+//}
 
 class ViewController: UIViewController {
     
     let titleLabel: UILabel = {
+        
         let label = UILabel()
         label.text = "스파르탕"
         label.font = UIFont.boldSystemFont(ofSize: 24)
@@ -38,12 +38,14 @@ class ViewController: UIViewController {
     }()
     
     let scrollView: UIScrollView = {
+        
         let scrollView = UIScrollView()
         scrollView.showsHorizontalScrollIndicator = false
         return scrollView
     }()
     
     let stackView: UIStackView = {
+        
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.spacing = 10
@@ -52,21 +54,27 @@ class ViewController: UIViewController {
     
     private let cart = CartManager()
     
+    let totalPriceLabel: UILabel = {
+        
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.textAlignment = .center
+        return label
+    }()
     
-    func addItemToCart(name: String, quantity: Int, price: Int) {
-        let food = Menu(name: name, quantity: quantity, price: price)
-        cart.menu.append(food)
+    func addItemToCart(menu: Menu) {
+        cart.menu.append(menu)
         collectionView.reloadData()
         calculateTotalPrice()
     }
-
+    
     func calculateTotalPrice() {
+        
         for item in cart.menu {
             cart.totalPrice += item.price * item.quantity
         }
         print("총 금액 : \(cart.totalPrice)")
     }
-    
     
     let tabs = ["장바구니", "안주", "탕", "술", "음료"]
     
@@ -78,27 +86,28 @@ class ViewController: UIViewController {
         ["d1", "d2", "d3", "d4", "d5", "d6", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil]
     ]
     
-    
     var selectedTabIndex = 0
     
     lazy var collectionView: UICollectionView = {
+        
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         layout.minimumInteritemSpacing = 0
         layout.minimumLineSpacing = 0
+        
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.backgroundColor = .clear
+        collectionView.backgroundColor = .white
         collectionView.register(ImageCell.self, forCellWithReuseIdentifier: ImageCell.reuseIdentifier)
         collectionView.dataSource = self
         collectionView.delegate = self
         return collectionView
     }()
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
     }
+    
     func setupViews() {
         view.backgroundColor = .white
         view.addSubview(titleLabel)
@@ -106,6 +115,7 @@ class ViewController: UIViewController {
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
             make.left.equalTo(view).offset(20)
         }
+        
         view.addSubview(scrollView)
         scrollView.snp.makeConstraints { make in
             make.top.equalTo(titleLabel.snp.bottom).offset(20)
@@ -118,7 +128,9 @@ class ViewController: UIViewController {
         stackView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
+        
         for (index, tab) in tabs.enumerated() {
+            
             let tabButton = UIButton()
             tabButton.setTitle(tab, for: .normal)
             tabButton.setTitleColor(.black, for: .normal)
@@ -135,6 +147,16 @@ class ViewController: UIViewController {
             stackView.addArrangedSubview(tabButton)
         }
         
+        view.addSubview(totalPriceLabel)
+        totalPriceLabel.snp.makeConstraints { make in
+            make.top.equalTo(scrollView.snp.bottom).offset(20)
+            make.left.equalTo(view).offset(20)
+            make.right.equalTo(view).offset(-20)
+            make.height.equalTo(50)
+        }
+        
+        totalPriceLabel.isHidden = true
+        
         view.addSubview(collectionView)
         collectionView.snp.makeConstraints { make in
             make.top.equalTo(scrollView.snp.bottom).offset(20)
@@ -148,16 +170,32 @@ class ViewController: UIViewController {
         selectedTabIndex = sender.tag
         collectionView.reloadData()
     }
+    
     func presentHalfScreenModal(with imageName: String) {
+        
         let modalViewController = ModalViewController()
         modalViewController.modalPresentationStyle = .pageSheet
         modalViewController.imageName = imageName
+        modalViewController.delegate = self
         present(modalViewController, animated: true, completion: nil)
     }
 }
 
+extension ViewController: ModalViewControllerDelegate{
+    
+    func updateCart(menu: Menu) {
+        addItemToCart(menu: menu)
+    }
+}
+
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
+    
     func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
         if selectedTabIndex == 0 {
             return 1
         } else {
@@ -165,16 +203,14 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFl
         }
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return images[selectedTabIndex].count
-    }
-    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCell.reuseIdentifier, for: indexPath) as! ImageCell
-        
-        if selectedTabIndex == 0 {
-            cell.imageView.image = UIImage(named: "cartImage")
+        if selectedTabIndex == 0 { cell.imageView.isHidden = true
+            cell.totalPriceLabel.isHidden = false
+            cell.totalPriceLabel.text = "총 금액 : \(cart.totalPrice)원"
         } else {
+            cell.imageView.isHidden = false
+            cell.totalPriceLabel.isHidden = true
             if let imageName = images[selectedTabIndex][indexPath.item], !imageName.isEmpty {
                 cell.imageView.image = UIImage(named: imageName)
             } else {
@@ -185,51 +221,68 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFl
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        if selectedTabIndex == 0 {
-            return CGSize(width: collectionView.frame.width, height: collectionView.frame.width)
+        if selectedTabIndex == 0 { return CGSize(width: collectionView.frame.width, height: 50)
         } else {
             let width = collectionView.frame.width / 2
             return CGSize(width: width, height: width)
         }
     }
     
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        if let imageName = images[selectedTabIndex][indexPath.item], !imageName.isEmpty {
-            presentHalfScreenModal(with: imageName)
-        }
-    }
-}
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) { if selectedTabIndex != 0 { if let imageName = images[selectedTabIndex][indexPath.item], !imageName.isEmpty { presentHalfScreenModal(with: imageName) } } } }
 
 class ImageCell: UICollectionViewCell {
+    
     static let reuseIdentifier = "ImageCell"
     
     let imageView: UIImageView = {
+        
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         imageView.backgroundColor = .white
+        
         return imageView
+    }()
+    
+    let totalPriceLabel: UILabel = {
+        
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.textAlignment = .center
+        label.isHidden = true
+        
+        return label
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         contentView.addSubview(imageView)
+        contentView.addSubview(totalPriceLabel)
+        
         imageView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
+        
+        totalPriceLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
         layer.borderColor = UIColor.white.cgColor
         layer.borderWidth = 5
     }
     
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 }
 
+protocol ModalViewControllerDelegate {
+    func updateCart(menu: Menu)
+}
 
 class ModalViewController: UIViewController {
+    
+    var delegate: ModalViewControllerDelegate?
     var imageName: String?
     var productName: String = "상품 이름"
     var price: Int = 10000
@@ -238,7 +291,9 @@ class ModalViewController: UIViewController {
             quantityLabel.text = "\(quantity)"
         }
     }
+    
     let imageView: UIImageView = {
+        
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
@@ -246,24 +301,28 @@ class ModalViewController: UIViewController {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
+    
     let nameLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.boldSystemFont(ofSize: 18)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
     let priceLabel: UILabel = {
         let label = UILabel()
         label.textColor = .gray
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
     let quantityLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 16)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
     let minusButton: UIButton = {
         let button = UIButton()
         button.setTitle("-", for: .normal)
@@ -273,6 +332,7 @@ class ModalViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
+    
     let plusButton: UIButton = {
         let button = UIButton()
         button.setTitle("+", for: .normal)
@@ -282,6 +342,7 @@ class ModalViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
+    
     let addToCartButton: UIButton = {
         let button = UIButton()
         button.setTitle("담기", for: .normal)
@@ -294,12 +355,15 @@ class ModalViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         if let sheetPresentationController = sheetPresentationController {
             sheetPresentationController.detents = [.medium(), .large()]
             setupViews()
         }
+        
         func setupViews() {
             view.backgroundColor = .white
+            
             let imageView = UIImageView()
             imageView.contentMode = .scaleAspectFill
             imageView.clipsToBounds = true
@@ -316,29 +380,29 @@ class ModalViewController: UIViewController {
             imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
             imageView.widthAnchor.constraint(equalToConstant: view.frame.width / 2).isActive = true
             imageView.heightAnchor.constraint(equalToConstant: view.frame.width / 2).isActive = true
-            // Name label setup
+
             view.addSubview(nameLabel)
             nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
             nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20).isActive = true
             nameLabel.text = productName
-            // Price label setup
+
             view.addSubview(priceLabel)
             priceLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
             priceLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
             priceLabel.text = "\(price)원"
-            // Quantity label setup
+
             view.addSubview(quantityLabel)
             quantityLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
             quantityLabel.topAnchor.constraint(equalTo: priceLabel.bottomAnchor, constant: 16).isActive = true
             quantityLabel.text = "\(quantity)"
-            // Minus button setup
+
             view.addSubview(minusButton)
             minusButton.centerYAnchor.constraint(equalTo: quantityLabel.centerYAnchor).isActive = true
             minusButton.trailingAnchor.constraint(equalTo: quantityLabel.leadingAnchor, constant: -16).isActive = true
             minusButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
             minusButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
             minusButton.addTarget(self, action: #selector(decreaseQuantity), for: .touchUpInside)
-            // Plus button setup
+
             view.addSubview(plusButton)
             plusButton.centerYAnchor.constraint(equalTo: quantityLabel.centerYAnchor).isActive = true
             plusButton.leadingAnchor.constraint(equalTo: quantityLabel.trailingAnchor, constant: 16).isActive = true
@@ -353,25 +417,26 @@ class ModalViewController: UIViewController {
             addToCartButton.addTarget(self, action: #selector(addToCart), for: .touchUpInside)
         }
         
-        
-        
-        // Tap gesture recognizer to dismiss modal view
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissModal))
         view.addGestureRecognizer(tapGesture)
     }
+    
     @objc func dismissModal() {
         dismiss(animated: true, completion: nil)
     }
+    
     @objc func increaseQuantity() {
         quantity += 1
     }
+    
     @objc func decreaseQuantity() {
         if quantity > 1 {
             quantity -= 1
         }
     }
+    
     @objc func addToCart(_ sender: UIButton) {
-
+        delegate?.updateCart(menu: Menu(name: productName, quantity: quantity, price: price))
         dismiss(animated: true, completion: nil)
     }
 }
