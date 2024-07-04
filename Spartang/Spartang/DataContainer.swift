5be604a797a732653538842b1176e927cb859454
ZiZi <gamdev_93@naver.com>
2024-07-04 10:56:36 +0900

    DataContainer init

---
+++ Spartang/Spartang/DataContainer.swift
@@ -0,0 +1,18 @@
+//
+//  DataContainer.swift
+//  Spartang
+//
+//  Created by 남지연 on 7/4/24.
+//
+
+import Foundation
+
+class DataContainer {
+    static let shared = DataContainer()
+    
+    private init() {
+        
+    }
+    
+    
+}
